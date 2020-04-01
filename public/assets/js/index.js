1a8fc6e4215e0633a8b8e00fc621c4ea047180a4
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-01 12:41:26 -0500

    added leaflet

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,34 @@
-const d3 = require('d3');
-const algolia = require('places.js');
+// Gets user coords in order to center map
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(res => {
+    const userCoords = [res.coords.latitude, res.coords.longitude];
 
-alert("test");
+    // Creates a new map using user coords
+    var mymap = L.map('mapid').setView(userCoords, 11);
+
+    // Styles newly created map
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/dark-v10',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiZ2FycmV0dG1yb2JlcnRzIiwiYSI6ImNrN2Rxcm4zYjAwbmkzZm50NXh5d211MnEifQ.QfPRfpUkyKGIjSdjfAzMkg'
+    }).addTo(mymap);
+  });
+} else {
+  alert('You need to enable location services for best app experience.');
+  // Creates a new map using generic coords
+  var userCoords = ['40.115791', '-100.603986'];
+  mymap = L.map('mapid').setView(userCoords, 4);
+
+  // Styles newly created map
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v10',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiZ2FycmV0dG1yb2JlcnRzIiwiYSI6ImNrN2Rxcm4zYjAwbmkzZm50NXh5d211MnEifQ.QfPRfpUkyKGIjSdjfAzMkg'
+  }).addTo(mymap);
+}
