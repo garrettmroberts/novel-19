a75945b4e49dbe44f46f860adf9447626a1dbee6
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-01 09:54:42 -0500

    add database/sequelize functionalities to server file

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-const express = require("express");
-
-const PORT = process.env.PORT || 8080
+const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log("Server listening on http://localhost:" + PORT))
+// Require models for syncing.
+var db = require('./models');
+
+
+// Sync application to models, then start the Express application.
+db.sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log('Server listening on PORT ' + PORT);
+  });
+});
