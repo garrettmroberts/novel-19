ae7ec73a10b0aada27f32b9cb5235decc9ac4e06
Bradley <bg9892@gmail.com>
2020-04-01 12:13:02 -0500

    added code for algolia places

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,4 +1,19 @@
-const d3 = require('d3');
-const algolia = require('places.js');
 
-alert("test");
+(function() {
+  var placesAutocomplete = places({
+    appId: 'pl9T9H0MC1H7',
+    apiKey: 'ca4802ec76ed8a2eab627fc4eae7ddbb',
+    container: document.querySelector('#address-input')
+  });
+
+  var $address = document.querySelector('#address-value');
+  placesAutocomplete.on('change', function(e) {
+    $address.textContent = e.suggestion.value;
+    console.log($address.textContent);
+  });
+
+  placesAutocomplete.on('clear', function() {
+    $address.textContent = 'none';
+  });
+
+})();
