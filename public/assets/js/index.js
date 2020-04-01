3c55aef03154b3c0a5641582f0769c41d57e1cbc
Garrett Roberts <garrettmroberts@gmail.com>
2020-04-01 10:15:41 -0500

    main page, server work

---
+++ public/assets/js/index.js
@@ -0,0 +1,4 @@
+const d3 = require('d3');
+const algolia = require('places.js');
+
+alert("test");
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.listen(PORT, () => console.log("Server listening on http://localhost:" + PORT))
+app.use(express.static("public"));
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(PORT, () => console.log("Server listening on http://localhost:" + PORT));
