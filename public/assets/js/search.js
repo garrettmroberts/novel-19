0c675750416077dfdd004d2d570e71a4a5f82365
Bradley <bg9892@gmail.com>
2020-04-02 10:54:26 -0500

    changed functions to arrow style

--- public/assets/js/search.js
+++ public/assets/js/search.js
@@ -1,20 +1,24 @@
 
 let coords;
 
+// Initialize the places library.
 const placesAutocomplete = places({
   appId: 'pl9T9H0MC1H7',
   apiKey: 'ca4802ec76ed8a2eab627fc4eae7ddbb',
   container: document.querySelector('#address-input')
 });
 
+// Gets value from input field and runs autocomplete to guess what the user is trying to type.
 const $address = document.querySelector('#address-value');
-placesAutocomplete.on('change', function (e) {
+placesAutocomplete.on('change', (e) => {
   $address.textContent = e.suggestion.value;
+  // Get the latlng coordinates from address typed.
   coords = e.suggestion.latlng;
   console.log(coords);
 });
 
-placesAutocomplete.on('clear', function () {
+// Clears autocomplete.
+placesAutocomplete.on('clear', () => {
   $address.textContent = 'none';
 });
 
