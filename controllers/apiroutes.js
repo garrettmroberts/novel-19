5feb00c6e52793a854df489865948d79e516b7b8
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-02 13:54:47 -0500

    simplify post /api/login route redirects. fiext res.redirect in post /api/signup

--- controllers/apiroutes.js
+++ controllers/apiroutes.js
@@ -7,9 +7,6 @@ module.exports = function(app) {
   // how the sequelize User model was configured. If the user is created successfully, proceed to
   // log the user in. Otherwise send back an error.
   app.post('/api/signup', (req, res) => {
-    console.log(req.body.username);
-    console.log(req.body.password);
-    console.log(req.body.yearBorn);
     db.User.create({
       username: req.body.username,
       password: req.body.password,
@@ -17,23 +14,18 @@ module.exports = function(app) {
       status: false
     })
       .then(() => {
-        console.log('inside /api/signup then');
-        res.redirect('api/login');
+        res.redirect('/api/login');
       })
       .catch((err) => {
-        console.log('inside /api/signup catch');
-        console.log(err);
         res.status(401).json(err);
       });
   });
 
   // Route to login. Uses passport.authenticate middleware that was set up with local strategy.
   // If the user has valid login credentials, sign them in. Otherwise send an error.
-  app.post('/api/login', passport.authenticate('local', { failureFlash: true }), (req, res) => {
-    res.json(req.user);
-  });
+  app.post('/api/login', passport.authenticate('local', { successRedirect: '/home', failureRedirect: '/home' }));
 
-  // Route to terminate a login session. According to passport docs, invoking req. logout() will 
+  // Route to terminate a login session. According to passport docs, invoking req. logout() will
   // remove the req.user property and clear the login session (if any).
   app.get('/logout', (req, res) => {
     req.logout();
