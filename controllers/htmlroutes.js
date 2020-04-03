e4585109abf8a81b1dcbea42d0436b646d248d8b
Tyler Kennedy <tkennedy118@gmail.com>
2020-04-03 18:45:45 -0500

    remove console.logs

--- controllers/htmlroutes.js
+++ controllers/htmlroutes.js
@@ -3,7 +3,6 @@ const isAuthenticated = require('../config/middleware/isAuthenticated');
 module.exports = function (app) {
   // Home page
   app.get('/home', (req, res) => {
-    console.log('/HOME');
     // User is signed in.
     if (req.user) {
       const data = {
@@ -12,11 +11,9 @@ module.exports = function (app) {
         status: (req.user.status === '1'),
         home: true
       };
-      console.log('IN IF, RES.RENDER INDEX: ', data);
       res.render('index', { user: data });
     }
     else {
-      console.log('IN ELSE, RES.RENDER INDEX: ');
       // User not signed in.
       res.status(206).render('index', { home: true});
     }
@@ -24,21 +21,18 @@ module.exports = function (app) {
 
   // Member page
   app.get('/profile', isAuthenticated, (req, res) => {
-    console.log('/PROFILE');
     const data = {
       username: req.user.username,
       yearBorn: req.user.yearBorn,
       status: (req.user.status === '1'),
       profile: true
     };
-    console.log('RES.RENDER PROFILE: ', data);
     res.render('profile', { user: data});
   });
 
 
   // Stats page
   app.get('/stats', (req, res) => {
-    console.log('RES.RENDER STATS');
     res.render('stats', { stats: true });
   });
 
